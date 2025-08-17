d8b9a1e2bfb324b590349bfc38135670efac051f
WeskleyReis <weskleyff88004216@gmail.com>
2025-08-17 16:37:53 -0300

    Refatora a view de receitas para incluir a criação de receitas com o autor definido.

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,13 +3,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
-from ..permissions import IsOwner
-from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import status
 
 from recipes.models import Recipe
 from tag.models import Tag
 from ..serializers import RecipeSerializer, TagSerializer
+from ..permissions import IsOwner
 
 
 class RecipeAPIv2Pagination(PageNumberPagination):
@@ -45,10 +45,19 @@ class RecipeAPIv2ViewSet(ModelViewSet):
         if self.request.method in ['PATCH', 'DELETE']:
             return [IsOwner(),]
         return super().get_permissions()
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
 
     def partial_update(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')
-
         recipe = self.get_object()
         serializer = RecipeSerializer(
             instance=recipe,
