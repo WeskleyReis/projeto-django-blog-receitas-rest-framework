1326b333c20bbf1895f13a329ca21432d3b2a913
WeskleyReis <weskleyff88004216@gmail.com>
2025-08-17 16:53:55 -0300

    Adiciona AuthorSerializer e AuthorViewSet à API de autores, incluindo rotas para gerenciamento de autores. Refatora urls e views para integrar novas funcionalidades.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import site
-from .views import api
+from . import views
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -13,54 +12,54 @@ app_name = 'recipes'
 recipe_api_v2_router = SimpleRouter()
 recipe_api_v2_router.register(
     'recipes/api/v2',
-    api.RecipeAPIv2ViewSet,
+    views.RecipeAPIv2ViewSet,
     basename='recipes-api'
 )
 
 urlpatterns = [
     path(
         '',
-        site.RecipeListViewHome.as_view(),
+        views.RecipeListViewHome.as_view(),
         name='home'
     ),
     path(
         'recipes/search/',
-        site.RecipeListViewSearch.as_view(),
+        views.RecipeListViewSearch.as_view(),
         name='search'
     ),
     path(
         'recipes/tags/<slug:slug>/',
-        site.RecipeListViewTag.as_view(),
+        views.RecipeListViewTag.as_view(),
         name="tag"
     ),
     path(
         'recipes/category/<int:category_id>/',
-        site.RecipeListViewCategory.as_view(),
+        views.RecipeListViewCategory.as_view(),
         name='category'
     ),
     path(
         'recipes/<int:pk>/',
-        site.RecipeDetail.as_view(),
+        views.RecipeDetail.as_view(),
         name='recipe'
     ),
     path(
         'recipes/api/v1/',
-        site.RecipeListViewHomeApi.as_view(),
+        views.RecipeListViewHomeApi.as_view(),
         name='api_v1'
     ),
     path(
         'recipes/api/v1/<int:pk>/',
-        site.RecipeDetailApi.as_view(),
+        views.RecipeDetailApi.as_view(),
         name='api_v1_detail'
     ),
     path(
         'recipes/theory/',
-        site.theory,
+        views.theory,
         name='theory',
     ),
     path(
         'recipes/api/v2/tag/<int:pk>/',
-        api.tag_api_detail,
+        views.tag_api_detail,
         name='recipes_api_v2_tag',
     ),
     path(
